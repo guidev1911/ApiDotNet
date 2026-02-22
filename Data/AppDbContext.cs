9da4eb0ee0587a3b006d98b1d1f824346117a3f8
guidev1911 <guilhermereal1911@gmail.com>
2026-02-21 23:46:37 -0300

    controller\db context, program.cs

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace ApiDotNet.Data
+﻿using ApiDotNet.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiDotNet.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios => Set<Usuario>();
     }
 }
