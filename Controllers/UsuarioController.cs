9c801e947b387109092cdea672ec4ffcdb774a37
guidev1911 <guilhermereal1911@gmail.com>
2026-02-26 23:50:57 -0300

    endpoint delete user

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,6 +1,48 @@
-﻿namespace ApiDotNet.Controllers
+﻿using ApiDotNet.Data;
+using ApiDotNet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiDotNet.Controllers
 {
-    public class UsuarioController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuariosController : ControllerBase
     {
+        private readonly AppDbContext _context;
+
+        public UsuariosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var usuarios = _context.Usuarios.ToList();
+            return Ok(usuarios);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Usuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+            _context.SaveChanges();
+
+            return Created("", usuario);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var usuario = _context.Usuarios.Find(id);
+
+            if (usuario == null)
+                return NotFound("Usuário não encontrado");
+
+            _context.Usuarios.Remove(usuario);
+            _context.SaveChanges();
+
+            return NoContent(); 
+        }
     }
 }
