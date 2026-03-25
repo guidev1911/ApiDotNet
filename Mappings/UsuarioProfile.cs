1ac56db0c96c567904ad52631e6b48b8777b92aa
guidev1911 <guilhermereal1911@gmail.com>
2026-03-24 23:11:18 -0300

    profile para autoMapper

---
+++ Mappings/UsuarioProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using ApiDotNet.Models;
+using ApiDotNet.DTOs;
+
+namespace ApiDotNet.Mappings
+{
+    public class UsuarioProfile : Profile
+    {
+        public UsuarioProfile()
+        {
+            // Entity -> Response
+            CreateMap<Usuario, UsuarioResponseDTO>();
+
+            // CreateDTO -> Entity
+            CreateMap<UsuarioCreateDTO, Usuario>();
+
+            // UpdateDTO -> Entity
+            CreateMap<UsuarioUpdateDTO, Usuario>();
+        }
+    }
+}
